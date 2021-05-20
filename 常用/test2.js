79404a58b0e0acb7a652962cc5a521c033acc887
蒋敏 <jiangmin@up366.com>
2021-05-20 18:35:43 +0800

    add：添加代码

--- 常用/test2.js
+++ 常用/test2.js
@@ -1,5 +1,30 @@
 /*jshint esversion: 6 */
-let f1 = function () {
-    console.log(1234)
+
+// 防抖函数
+function debounce(fn, wait, immediate) {
+    if(typeof fn !== 'function')throw TypeError()
+    let timer, result;
+    let deb = function () {
+        if (timer) clearTimeout(timer);
+        if (immediate) {
+            if (!timer) {
+                result = fn.apply(this, arguments);
+            }
+        }
+        timer = setTimeout(() => {
+            result = fn.apply(this, arguments)
+            timer = null
+        }, wait);
+        return result
+    }
+    deb.cancel = () => {
+        clearTimeout(timer);
+        timer = null
+    }
+    return deb
 }
-console.log('test1.2')
+var f3 = debounce(function (haha) {
+    console.log(haha)
+},500,true)
+f3(1234);
+
